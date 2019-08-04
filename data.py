11e0b64840eab132ca4df1b33d3ed6a470a61646
Anuj Yadav <anuj293@gmail.com>
2019-08-04 22:57:15 +0530

    1. created two methods set_game_active() and set_game_inactive() so that I can control the display of mouse cursor

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-
+import pygame
 
 class Data:
     """The class is to store game data"""
@@ -6,7 +6,7 @@ class Data:
     def __init__(self, ai_settings):
         """initialize the game data"""
         self.ai_settings = ai_settings
-        self.numberofships = 4
+        self.ship_left = self.ai_settings.life_limit
         self.reset_data()
         # start the game in inactive state
         self.is_game_active = False
@@ -15,3 +15,15 @@ class Data:
     def reset_data(self):
         """reset the game data that can change during runtime"""
         self.ship_left = self.ai_settings.life_limit
+
+    def set_game_inactive(self):
+        """set the flag is_game_active = False and hide the cursor"""
+        self.is_game_active = False
+        # show the mouse cursor
+        pygame.mouse.set_visible(True)
+
+    def set_game_active(self):
+        """set the flag is_game_active = False and hide the cursor"""
+        self.is_game_active = True
+        # show the mouse cursor
+        pygame.mouse.set_visible(False)
